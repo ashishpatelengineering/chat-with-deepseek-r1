037cec14af819d069c12ac157d90521c9a8157f7
ashishpatelengineering <114289015+ashishpatelengineering@users.noreply.github.com>
2025-02-03 00:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ message_templates = [
 ]
 
 def search(query, client, embed_model, k=5):
-    collection_name = "bhagavad-gita"
+    collection_name = "bhagavad-gita-3"
     query_embedding = embed_model.get_query_embedding(query)
     result = client.query_points(
         collection_name=collection_name,
